f521c8d64e8a126be62962f4dddcec4affb2a6e6
ICEon <matrix_eon@yahoo.com>
2015-11-22 12:05:17 -0600

    uno

---
+++ js/acciones.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+var elementoEncontrar = 0;
+var aciertos = 0;
+var db;
+
+//*************************
+
+
+function conectar_base()
+ {
+  db = window.sqlitePlugin.openDatabase({name: "quimica_b.db", createFromLocation: 1});					
+ }
+ 
+ 
+$(document).ready(function(e) {
+	
+document.addEventListener("deviceready", onDeviceReady, false);
+ function onDeviceReady() {
+	 
+ conectar_base();
+
+ $('#juagar').on('tap', function(){
+
+    elementoEncontrar = Math.floor((Math.random() * 118) + 1);
+	  db.transaction(function(tx) {
+        tx.executeSql("select numeroAtomico, nombreElemento from Elementos where numeroAtomico = " + elementoEncontrar + ";", [], function(tx, res) {
+			$("#elementoEncontrar").html(res.rows.item(0).nombreElemento);
+        });
+      });
+	$(':mobile-pagecontainer').pagecontainer('change', '#juego',{
+            transition: 'pop'
+			}); 
+  $("#quien").popup();
+  $("#quien").popup("open");	 
+
+
+ });
+ 
+ 
+ }
+
+
+});
+
