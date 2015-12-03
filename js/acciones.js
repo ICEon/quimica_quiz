afe935005d732131b327872dcaaa0a77a4b29714
ICEon <matrix_eon@yahoo.com>
2015-12-03 05:11:24 -0600

    minus 1

--- js/acciones.js
+++ js/acciones.js
@@ -1,65 +1,40 @@
 // JavaScript Document
-var elementoEncontrar = 0;
-var aciertos = 0;
-var db;
-
-//*************************
-
-
-function conectar_base()
- {
-  db = window.sqlitePlugin.openDatabase({name: "quimica_b.db", createFromLocation: 1});					
- }
- 
  
 $(document).ready(function(e) {
+	ancho = $('#principal').width();	 
+	alto = $('body').height();	
+			 velocidad = 1500 -(alto*.4);
+    $('.boton').css('width', ancho*.20);
+	$('.boton').css('height', ancho*.20);
+    $('.boton').css('font-size', ancho*.15);
+	$('.carril').css('width', ancho*.20);
+    $('.carril').css('height', alto*.75);
+	$('.adivinar').css('height', ancho*.20);
 	
-document.addEventListener("deviceready", onDeviceReady, false);
-
-$('#bnterror').on('tap', function(){
-  $("#quien").popup("close");
-  $('#error').popup('open');   		
-});
-
-$('#btnacierto').on('tap', function(){
-	  $("#quien").popup("close");
-  $('#acierto').popup('open');   		
-});
-
- function onDeviceReady() {
+    	
+    $('#tablero span').css('font-size', ancho*.11);
+	
+		alto_footer = $('#comandos').height();
+	
+//	alert (alto + "footer " + alto_footer);
+		
+ $('.boton').on('tap', function(){
+	alert (velocidad); 
 	 
- conectar_base();
+	 $('#uno').css('top', $('#uno').position().top -100);
+	 	 $('#tres').css('top', $('#tres').position().top -50);
+		 	 	 $('#dos').css('top', $('#dos').position().top -10);
 
- $('#btnjugar').on('tap', function(){
-					  $("#quien").popup();
-    elementoEncontrar = Math.floor((Math.random() * 118) + 1);
-//	alert (elementoEncontrar);
-  db.transaction(function(tx) {
-        tx.executeSql("select numeroAtomico, nombreElemento from Elementos where numeroAtomico = " + elementoEncontrar + ";", [], function(tx, res) {
-			$("#elementoEncontrar").html(res.rows.item(0).nombreElemento);
-        });
-      });
-	$(':mobile-pagecontainer').pagecontainer('change', '#juego',{
-            transition: 'pop'
-			}); 
-			
+$("#tablero span")
+//.css({top:0,position:'relative'})
+.animate({top: alto*.78}, velocidad, function() {
+	var position = $('#uno').position();
+//alert ( position.top );
+    //callback
 
-
-  $("#quien").popup("open",{transition: "flip"});	 
-
-
-
-
-
-
+});
 
  });
- 
- 
- }
-
-
-
-
+		
 });
 
